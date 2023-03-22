fca25d234407a1da81ec6b8880d6ada278b60ae3
Kade Fraser <kadejfraser@gmail.com>
2023-03-22 16:15:56 -0700

    Create profile.js for the user profile and added back the loadReportsList retitled as loadUsreReportsList.

---
+++ scripts/profile.js
@@ -0,0 +1,24 @@
+
+
+
+function loadUserReportsList() {
+  firebase.auth().onAuthStateChanged((user) => {
+    if (user) {
+      db.collection("users")
+        .doc(user.uid)
+        .get()
+        .then((user) => {
+          var reports = user.data().reports;
+          reports.forEach((repID) => {
+            db.collection("hazards")
+              .doc(repID)
+              .get()
+              .then((report) => {
+                let title = report.data().title;
+                createHTMLReport(title);
+              });
+          });
+        });
+    }
+  });
+}
