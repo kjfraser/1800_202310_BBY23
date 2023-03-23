0481a3359622b54a528baa222748253ddae8f267
Emily Yao <emilyyao9898_@hotmail.com>
2023-03-22 17:06:22 -0700

    link profile html ajs to firebase

--- scripts/profile.js
+++ scripts/profile.js
@@ -1,5 +1,38 @@
 
+var currentUser;          //put this right after you start script tag before writing any functions.
 
+function populateUserInfo() {
+    firebase.auth().onAuthStateChanged(user => {
+        // Check if user is signed in:
+        if (user) {
+
+            //go to the correct user document by referencing to the user uid
+            currentUser = db.collection("users").doc(user.uid)
+            //get the document for current user.
+            currentUser.get()
+                .then(userDoc => {
+                    //get the data fields of the user
+                    var userName = userDoc.data().name;
+                    var userEmail = userDoc.data().email;
+                   
+
+                    //if the data fields are not empty, then write them in to the form.
+                    if (userName != null) {
+                        document.getElementById("nameInput").value = userName;
+                    }
+                    if (userSchool != null) {
+                        document.getElementById("emailInput").value = userEmail;
+                    }
+                    
+                })
+        } else {
+            // No user is signed in.
+            console.log ("No user is signed in");
+        }
+    });
+}
+
+populateUserInfo();
 
 function loadUserReportsList() {
   firebase.auth().onAuthStateChanged((user) => {
